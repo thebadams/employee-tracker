e56b78df93f6469e9702ed314f22ab9b9214f938
Brian Adams <bpeteradams@gmail.com>
2021-04-27 15:02:12 -0400

    wrote basic select statement functions to get all information from database tables

--- database.js
+++ database.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const inquirer = require('mysql');
+const cTable = require("console.table");
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -10,8 +11,37 @@ const connection = mysql.createConnection({
 });
 
 
+
+// function to select all employees
+const getEmployeeInfo = () => {
+    connection.query("SELECT * FROM employees", (err, res)=>{
+        if(err) throw err;
+        console.table(res);
+    })
+}
+
+const getDepartmentInfo = () => {
+    connection.query("SELECT * FROM departments", (err, res)=>{
+        if(err) throw err;
+        console.table(res);
+    })
+}
+
+const getRoleInfo = () => {
+    connection.query('SELECT * FROM roles', (err, res)=>{
+        if(err) throw err;
+        console.table(res);
+    })
+}
+
+const endConnection = () =>{
+    connection.end();
+}
 connection.connect((err)=>{
     if(err) throw err;
     console.log(`connected as id ${connection.threadId}`)
-    // afterConnection();
+    getEmployeeInfo();
+    getDepartmentInfo();
+    getRoleInfo();
+    endConnection();
 })
