014e9a324d54ca861e12e36b338c18efcc36a0ea
Brian Adams <bpeteradams@gmail.com>
2021-04-27 16:19:52 -0400

    converted database information to OOP

--- database.js
+++ database.js
@@ -2,13 +2,75 @@ const mysql = require('mysql');
 const inquirer = require('mysql');
 const cTable = require("console.table");
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Bp@olo21992',
-    database: 'company_db',
-});
+
+class Database{
+    constructor(host = 'localhost', port = 3306, user = 'root', password = 'Bp@olo21992', database = 'company_db', tables = {
+        employeeTable : 'employees',
+        roleTable : 'roles',
+        departmentTable : 'departments'
+    }){
+        this.host = host,
+        this.port = port,
+        this.user = user,
+        this.password = password,
+        this.database = database
+        this.tables = tables
+    }
+    // database creation functions
+    createDBConnection(){
+        this.connection = mysql.createConnection({
+            host: this.host,
+            port: this.port,
+            user: this.user,
+            password: this.password,
+            database: this.database
+        })
+    }
+    connectToDB(){
+        this.connection.connect((err)=>{
+    if(err) throw err;
+    console.log(`connected as id ${this.connection.threadId}`)
+    });
+    }
+    disconnectFromDB(){
+        this.connection.end();
+    }
+    // get information functions
+    displayEmployeesTable(){
+        this.connection.query(`SELECT * FROM ${this.tables.employeeTable}`, (err, res)=>{
+            if(err) throw err;
+            console.table(res);
+        })
+    }
+    displayDepartmentsTable(){
+        this.connection.query(`SELECT * FROM ${this.tables.departmentTable}`, (err, res)=>{
+            if(err) throw err;
+            console.table(res);
+        })
+    }
+    displayRolesTable(){
+        this.connection.query(`SELECT * FROM ${this.tables.roleTable}`, (err,res)=>{
+            if(err) throw err;
+            console.table(res);
+        })
+    }
+}
+
+const database = new Database()
+
+// database.createDBConnection();
+// database.connectToDB();
+// database.displayEmployeesTable();
+// database.displayDepartmentsTable();
+// database.displayRolesTable();
+// database.disconnectFromDB();
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: 'Bp@olo21992',
+//     database: 'company_db',
+// });
 
 
 
@@ -37,11 +99,13 @@ const getRoleInfo = () => {
 const endConnection = () =>{
     connection.end();
 }
-connection.connect((err)=>{
-    if(err) throw err;
-    console.log(`connected as id ${connection.threadId}`)
-    getEmployeeInfo();
-    getDepartmentInfo();
-    getRoleInfo();
-    endConnection();
-})
+// connection.connect((err)=>{
+//     if(err) throw err;
+//     console.log(`connected as id ${connection.threadId}`)
+//     getEmployeeInfo();
+//     getDepartmentInfo();
+//     getRoleInfo();
+//     endConnection();
+// })
+
+module.exports = database;
