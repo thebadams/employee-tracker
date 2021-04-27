0fa0f1b24720d8ccb7eb51a346f32616eff6143c
Brian Adams <bpeteradams@gmail.com>
2021-04-27 12:27:47 -0400

    wrote roles-seed.sql data

---
+++ schema/roles-seed.sql
@@ -0,0 +1,13 @@
+INSERT INTO roles(title, salary, department_id)
+VALUES("Senior Engineer", 100000, 3),
+("Junior Engineer", 65000, 3),
+("Project Manager", 125000, 3),
+("Senior Sales Account Manager", 70000, 4),
+("Junior Sales Account Manager", 45000, 4),
+("Director of Sales", 100000, 4),
+("Senior Counsel", 100000, 1),
+("Junior Counsel", 65000, 1),
+("Director of Legal Affairs", 150000, 1),
+("Human Resources Administrator", 40000, 2),
+("Human Resources Manager", 70000, 2),
+("Director of Human Resources", 125000, 2);
