3e6e08d85a1e1dcfe2bcef434927d648e701254e
Brian Adams <bpeteradams@gmail.com>
2021-05-01 16:09:36 -0400

    wrote functions to gather information for gathering information when creating new rows for tables

--- app-prompts.js
+++ app-prompts.js
@@ -2,35 +2,105 @@ const inquirer = require('inquirer');
 const database = require('./database')
 
 
-const startApp= async ()=> {
-        return inquirer.prompt({
-            type: 'list',
-            message: 'What Would You Like To Do',
-            name: 'startMenu',
-            choices: ['View Employee Table', 'View Roles Table', 'View Departments Table']
-        }).then((data)=>data.startMenu)
+const startApp = async () => {
+    const  startMenu = await inquirer.prompt({
+        type: "list",
+        name: "startMenuChoice",
+        choices: ["Add Department", "Add Role", "Add Employee"],
+        message: "Please Choose What You Would Like To Do."
+    })
+    const userChoice = startMenu.startMenuChoice;
 
-    }
+    return userChoice
+}
 
-        
-
-const checkMenuInput = async (input) =>{
-    switch(input){
-        case 'View Employee Table':
-            database.displayEmployeesTable();
+const checkUserChoice = async (userChoice) =>{
+    switch(userChoice){
+        case "Add Department":
+            gatherDepartmentInfo();
             break;
-        case 'View Roles Table':
-            database.displayRolesTable();
+        case "Add Role":
+            gatherRoleInfo();
             break;
-        case 'View Departments Table':
-            database.displayDepartmentsTable();
-
+        case "Add Employee":
+            gatherEmployeeInfo();
+            break;
+        default:
+            console.log("Nothing Chosen");
     }
 }
 
-const init = () =>{
-    database.createDBConnection();
-    startApp().then((input)=>checkMenuInput(input)).then(()=>database.disconnectFromDB());
+const gatherDepartmentInfo = async ()=>{
+    const deptInfo = await inquirer.prompt({
+        type: "input",
+        name: "deptName",
+        message: "Please input the new department's name."
+    })
+    const deptName = deptInfo.deptName;
+    console.log(deptName);
+}
+
+const gatherRoleInfo = async ()=>{
+    const roleInfo = await inquirer.prompt([
+        {
+        type: "list",
+        choices: ["Department 1", "Department 2", "Department 3", "Department 4"],
+        message: "Please Choose Which Department To Add this Role Into",
+        name: "roleDept"
+        },
+        {
+            type: "input",
+            message: "Please give a name for the new role.",
+            name: "roleTitle"
+        },
+        {
+            type: "input",
+            message:"Please give a salary for the new role",
+            name: "roleSalary"
+        }
+    ])
+    roleInfo.roleSalary = parseInt(roleInfo.roleSalary)
+    roleInfo.roleTitle = roleInfo.roleTitle.trim()
+
+    console.log(roleInfo);
+}
+
+const gatherEmployeeInfo = async ()=>{
+    const employeeInfo = await inquirer.prompt([
+        {
+           type: "list",
+           choices: ["Role 1", "Role 2", "Role 3", "Role 4"],
+           name: "employeeRole",
+           message: "Please Choose and Employee Role"
+        },
+        {
+            type: "input",
+            message: "Please give the Employee's First Name",
+            name: "employeeFirstName",
+        },
+        {
+            type: "input",
+            message: "Please give the employee's last name",
+            name: "employeeLastName"
+        },
+        {
+            type: "list",
+            choices: ["Manager 1", "Manager 2", "Manager 3", "No Manager"],
+            name: "employeeManager"
+        }
+    ])
+    employeeInfo.employeeFirstName = employeeInfo.employeeFirstName.trim();
+    employeeInfo.employeeLastName = employeeInfo.employeeLastName.trim();
+    if(employeeInfo.employeeManager==="No Manager"){
+        employeeInfo.employeeManager = null;
+    }
+    console.log(employeeInfo)
+}
+
+const init = async () => {
+    const userChoice = await startApp();
+    const checkMenuChoice = await checkUserChoice(userChoice);
+    return checkMenuChoice;
 }
 
 init()
--- database.js
+++ database.js
@@ -1,134 +1,165 @@
 const mysql = require('mysql');
 const inquirer = require('mysql');
-const cTable = require("console.table");
+const cTable = require('console.table');
 
+class Database {
+	constructor(
+		host = 'localhost',
+		port = 3306,
+		user = 'root',
+		password = 'Bp@olo21992',
+		database = 'company_db',
+		tables = {
+			employeeTable: 'employees',
+			roleTable: 'roles',
+			departmentTable: 'departments'
+		}
+	) {
+		(this.host = host),
+			(this.port = port),
+			(this.user = user),
+			(this.password = password),
+			(this.database = database);
+		this.tables = tables;
+	}
+	// database creation functions
+	async createDBConnection() {
+		return (this.connection = mysql.createConnection({
+			host: this.host,
+			port: this.port,
+			user: this.user,
+			password: this.password,
+			database: this.database
+		}));
+	}
+	async connectToDB() {
+		return this.connection.connect((err) => {
+			if (err) throw err;
+			console.log(`connected as id ${this.connection.threadId}`);
+		});
+	}
+	async disconnectFromDB() {
+		return this.connection.end();
+	}
+	// get information functions
+	async displayEmployeesTable() {
+		//this.connectToDB();
+		return this.connection.query(`SELECT * FROM ${this.tables.employeeTable}`, (err, res) => {
+			if (err) throw err;
+			console.table(res);
+		});
+	}
+	async displayDepartmentsTable() {
+		return this.connection.query(`SELECT * FROM ${this.tables.departmentTable}`, (err, res) => {
+			if (err) throw err;
+			console.table(res);
+		});
+	}
+	async displayRolesTable() {
+		return this.connection.query(`SELECT * FROM ${this.tables.roleTable}`, (err, res) => {
+			if (err) throw err;
+			console.table(res);
+		});
+	}
 
-class Database{
-    constructor(host = 'localhost', port = 3306, user = 'root', password = 'Bp@olo21992', database = 'company_db', tables = {
-        employeeTable : 'employees',
-        roleTable : 'roles',
-        departmentTable : 'departments'
-    }){
-        this.host = host,
-        this.port = port,
-        this.user = user,
-        this.password = password,
-        this.database = database
-        this.tables = tables
-    }
-    // database creation functions
-    createDBConnection(){
-        this.connection = mysql.createConnection({
-            host: this.host,
-            port: this.port,
-            user: this.user,
-            password: this.password,
-            database: this.database
-        })
-    }
-    connectToDB(){
-        this.connection.connect((err)=>{
-    if(err) throw err;
-    console.log(`connected as id ${this.connection.threadId}`)
-    });
-    }
-    disconnectFromDB(){
-        this.connection.end();
-    }
-    // get information functions
-    displayEmployeesTable(){
-        //this.connectToDB();
-        this.connection.query(`SELECT * FROM ${this.tables.employeeTable}`, (err, res)=>{
-            if(err) throw err;
-            console.table(res);
-        })
-    }
-    displayDepartmentsTable(){
-        this.connection.query(`SELECT * FROM ${this.tables.departmentTable}`, (err, res)=>{
-            if(err) throw err;
-            console.table(res);
-        })
-    }
-    displayRolesTable(){
-        this.connection.query(`SELECT * FROM ${this.tables.roleTable}`, (err,res)=>{
-            if(err) throw err;
-            console.table(res);
-        })
-    }
+	// get tables raw information
+	async getEmployeesTable() {
+		//this.connectToDB();
+		return this.connection.query(`SELECT * FROM ${this.tables.employeeTable}`, (err, res) => {
+			if (err) throw err;
+			return res;
+		});
+	}
 
-    // get tables raw information
-    getEmployeesTable(){
-        //this.connectToDB();
-        this.connection.query(`SELECT * FROM ${this.tables.employeeTable}`, (err, res)=>{
-            if(err) throw err;
-            return res;
-        })
-    }
+	async getDepartmentsTable() {
+		return this.connection.query(`SELECT * FROM ${this.tables.departmentTable}`, (err, res) => {
+			if (err) throw err;
+			return res;
+		});
+	}
 
-    getDepartmentsTable(){
-        this.connection.query(`SELECT * FROM ${this.tables.departmentTable}`, (err, res)=>{
-            if(err) throw err;
-            return res;
-        })
-    }
+	async getRolesTable() {
+		return this.connection.query(`SELECT * FROM ${this.tables.roleTable}`, (err, res) => {
+			if (err) throw err;
+			return res;
+		});
+	}
 
-    getRolesTable(){
-        this.connection.query(`SELECT * FROM ${this.tables.roleTable}`, (err,res)=>{
-            if(err) throw err;
-            return res;
-        })
-    }
+	// insert new rows
 
-    // insert new rows
-    
-    addNewDepartment(departmentName){
-         this.connection.query(`INSERT INTO ${this.tables.departmentTable}(name) VALUES('${departmentName}')`, (err, res)=>{
-             if(err) throw err;
-            console.log("Successfully Added")
-         })
-    }
+	async addNewDepartment(departmentName) {
+		return this.connection.query(
+			`INSERT INTO ${this.tables.departmentTable}(name) VALUES('${departmentName}')`,
+			(err, res) => {
+				if (err) throw err;
+				console.log('Successfully Added');
+			}
+		);
+	}
 
-    addNewRole(roleTitle, salary, department_id) {
-        this.connection.query(`INSERT INTO ${this.tables.roleTable}(title, salary, department_id) VALUES('${roleTitle}', ${salary}, ${department_id})`, (err, res)=>{
-            if(err) throw err;
-            console.log("Successfully Added");
-        })
-    }
+	async addNewRole(roleTitle, salary, department_id) {
+		return this.connection.query(
+			`INSERT INTO ${this.tables
+				.roleTable}(title, salary, department_id) VALUES('${roleTitle}', ${salary}, ${department_id})`,
+			(err, res) => {
+				if (err) throw err;
+				console.log('Successfully Added');
+			}
+		);
+	}
 
-    addNewEmployee(first_name, last_name, role_id, manager_id){
-        this.connection.query((`INSERT INTO ${this.tables.employeeTable} VALUES('${first_name}', '${last_name}', ${role_id}, ${manager_id})`), (err, res)=>{
-            if(err) throw err
-            console.log("Success")
-        })
-    }
-    updateEmployee(firstName, lastName, roleID, managerID, whereClause){
-        this.connection.query(`UPDATE ${this.tables.employeeTable} SET  first_name = '${firstName}', last_name = '${lastName}', role_id =${roleID}, manager_id=${managerID} WHERE ?`, whereClause,  (err, res)=>{
-            if(err) throw err;
-            console.log("Successfully Updated")
-        })
-    }
-    updateRole(title, department_id, salary, whereClause){
-        this.connection.query(`UPDATE ${this.tables.roleTable} SET title='${title}', salary=${salary}, department_id=${department_id} WHERE ?`, whereClause, (err, res)=>{
-            if(err) throw err;
-            console.log("Successfully Updated")
-        })
-    }
+	async addNewEmployee(first_name, last_name, role_id, manager_id) {
+		return this.connection.query(
+			`INSERT INTO ${this.tables
+				.employeeTable} VALUES('${first_name}', '${last_name}', ${role_id}, ${manager_id})`,
+			(err, res) => {
+				if (err) throw err;
+				console.log('Success');
+			}
+		);
+	}
+	async updateEmployee(firstName, lastName, roleID, managerID, whereClause) {
+		return this.connection.query(
+			`UPDATE ${this.tables
+				.employeeTable} SET  first_name = '${firstName}', last_name = '${lastName}', role_id =${roleID}, manager_id=${managerID} WHERE ?`,
+			whereClause,
+			(err, res) => {
+				if (err) throw err;
+				console.log('Successfully Updated');
+			}
+		);
+	}
+	async updateRole(title, department_id, salary, whereClause) {
+		return this.connection.query(
+			`UPDATE ${this.tables
+				.roleTable} SET title='${title}', salary=${salary}, department_id=${department_id} WHERE ?`,
+			whereClause,
+			(err, res) => {
+				if (err) throw err;
+				console.log('Successfully Updated');
+			}
+		);
+	}
 
-    updateDepartment(name, whereClause){
-        this.connection.query(`UPDATE ${this.tables.departmentTable} SET name='${name} WHERE ?`, whereClause, (err, res)=>{
-            if(err) throw err;
-            console.log("Successfully Updated")
-        })
-    }
+	async updateDepartment(name, whereClause) {
+		return this.connection.query(
+			`UPDATE ${this.tables.departmentTable} SET name='${name} WHERE ?`,
+			whereClause,
+			(err, res) => {
+				if (err) throw err;
+				console.log('Successfully Updated');
+			}
+		);
+	}
 }
 
-const database = new Database()
+const database = new Database();
 
-database.createDBConnection();
-// database.addNewDepartment('Administration');
-database.addNewRole('Secratary', 30000, 6);
-// database.addNewEmployee('George', 'Lopez', 14)
-database.disconnectFromDB();
+// database.createDBConnection();
+// // database.addNewDepartment('Administration');
+// database.addNewRole('Secratary', 30000, 6);
+// // database.addNewEmployee('George', 'Lopez', 14)
+// database.disconnectFromDB();
 // database.connectToDB();
 //database.displayEmployeesTable();
 // database.displayDepartmentsTable();
@@ -142,33 +173,31 @@ database.disconnectFromDB();
 //     database: 'company_db',
 // });
 
-
-
 // function to select all employees
 const getEmployeeInfo = () => {
-    connection.query("SELECT * FROM employees", (err, res)=>{
-        if(err) throw err;
-        console.table(res);
-    })
-}
+	connection.query('SELECT * FROM employees', (err, res) => {
+		if (err) throw err;
+		console.table(res);
+	});
+};
 
 const getDepartmentInfo = () => {
-    connection.query("SELECT * FROM departments", (err, res)=>{
-        if(err) throw err;
-        console.table(res);
-    })
-}
+	connection.query('SELECT * FROM departments', (err, res) => {
+		if (err) throw err;
+		console.table(res);
+	});
+};
 
 const getRoleInfo = () => {
-    connection.query('SELECT * FROM roles', (err, res)=>{
-        if(err) throw err;
-        console.table(res);
-    })
-}
+	connection.query('SELECT * FROM roles', (err, res) => {
+		if (err) throw err;
+		console.table(res);
+	});
+};
 
-const endConnection = () =>{
-    connection.end();
-}
+const endConnection = () => {
+	connection.end();
+};
 // connection.connect((err)=>{
 //     if(err) throw err;
 //     console.log(`connected as id ${connection.threadId}`)
